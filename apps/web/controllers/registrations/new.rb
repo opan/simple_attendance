a6b46f1e8c7ee41a51c571eacf11b2bbde132779
opanmustopah <opan.neutron@gmail.com>
2017-08-27 15:18:17 +0700

    Redirect to dashboard when user access login/ sign up pages if already signed in

--- apps/web/controllers/registrations/new.rb
+++ apps/web/controllers/registrations/new.rb
@@ -3,6 +3,10 @@ module Web::Controllers::Registrations
     include Web::Action
 
     def call(params)
+      if authenticated?
+        flash[:warning] = "You've already signed in!"
+        redirect_to routes.dashboards_path
+      end
     end
   end
 end
--- apps/web/views/application_layout.rb
+++ apps/web/views/application_layout.rb
@@ -1,10 +1,15 @@
+# frozen_string_literal: true
+
 module Web
   module Views
     class ApplicationLayout
       include Web::Layout
 
       def flash_messages(flash)
-        alert = [{ class: 'danger', type: :alert }, { class: 'info', type: :info }].find { |a| !flash[a[:type]].nil? }
+        alert = [
+          { class: 'danger', type: :alert }, { class: 'info', type: :info },
+          { class: 'warning', type: :warning },
+        ].find { |a| !flash[a[:type]].nil? }
 
         html.div(class: "alert alert-#{alert[:class]} alert-dismissable text-center", role: 'alert') do
           button(class: 'close', type: 'button', 'data-dismiss' => 'alert', 'aria-label' => 'close') do
