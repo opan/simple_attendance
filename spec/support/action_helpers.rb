166eaa42a80c9729d236fdb98e33065eb139e9ff
opanmustopah <opan.neutron@gmail.com>
2017-09-03 16:13:43 +0700

    Remove comment action_helpers

--- spec/support/action_helpers.rb
+++ spec/support/action_helpers.rb
@@ -1,42 +1,5 @@
 module ActionHelpers
   def sign_in(action:, user: double('User'))
-    # config = Warden::Config.new
-    # config.failure_app = Web::Controllers::Sessions::Unauthenticated
-    # config.default_strategies :password
-    # config.default_scope = :user
-    # config.scope_defaults :user, strategies: [:password], store: true, action: 'sessions/unauthenticated'
-    # # config.merge!({
-      # default_strategies: :password,
-    # })
-    # config = Warden::Config.new({
-      # default_scope: :user,
-      # scope_defaults: {
-        # user: {
-          # store: true, action: 'sessions/unauthenticated',
-          # strategies: [:password],
-        # },
-      # },
-      # default_strategies: [:password],
-    # })
-
-    # Warden::Manager.before_failure do |env, opts|
-      # binding.pry
-      # # env['REQUEST_METHOD'] = 'POST'
-      # # env['AUTH_VALID'] = false
-    # end
-
-    # Warden::Strategies.add(:password) do
-      # def valid?
-        # false
-      # end
-
-      # def authenticate!
-        # fail!('Failed test')
-      # end
-    # end
-
-    # warden = Warden::Proxy.new({}, Warden::Manager.new(Hanami.app, config))
-    # binding.pry
     warden = double('Warden')
     allow(action.send(:request)).to receive(:env).and_return({ 'warden' => warden })
     allow(warden).to receive(:user) { user }
@@ -45,7 +8,7 @@ module ActionHelpers
       allow(warden).to receive(:authenticate!).and_throw(:warden)
       allow(warden).to receive(:authenticated?) { false }
     else
-      allow(warden).to receive(:authenticate!).and_throw(:warden)
+      allow(warden).to receive(:authenticate!)
       allow(warden).to receive(:authenticated?) { true }
     end
   end
